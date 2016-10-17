1799c33d249a757e11d391c00aae6b0988e6cb5a
vivian.mzh@gmail.com <vivian.mzh@gmail.com>
2016-10-16 20:26:06 -0700

    finished

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var results = [];
+  function search(element) {
+    if (element.classList.contains(className)) {
+      results.push(element);
+    }
+    for (var i = 0; i < element.children.length; i++) {
+      search(element.children[i]);
+    }
+  }
+  search(document.body);
+  return results;
 };
+
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,35 @@
 
 // but you don't so you're going to write it from scratch:
 
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+ if (typeof obj === 'object' && obj !== null) {
+   if (Array.isArray(obj)) {
+     if (obj.length === 0) {
+       return '[]';
+     } else {
+       var stringify = [];
+       obj.forEach(function (val) {
+         stringify.push(stringifyJSON(val));
+       })
+       return '[' + stringify + ']';
+     }
+   } else {
+       var objectArray = [];
+       for (var key in obj) {
+         if (obj[key] === undefined || typeof obj[key] === 'function') {
+             return '{}';
+         } else {
+             objectArray.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+           }
+       }
+       return '{' + objectArray + '}';
+
+   }
+ } else if (typeof obj === 'string'){
+   return '"' + obj + '"';
+ } else {
+   return '' + obj;
+ }
 };
